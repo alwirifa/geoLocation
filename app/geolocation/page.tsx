bb3f0c9eda7efc20f56ddbb6cf5aa83069cf4304
Mochamad ilham alwi rifa <95983472+alwirifa@users.noreply.github.com>
2024-03-21 13:55:58 +0700

    fixed testing

--- app/geolocation/page.tsx
+++ app/geolocation/page.tsx
@@ -2,8 +2,6 @@
 
 import React, { useState, useEffect } from "react";
 import YouTube from "react-youtube";
-import L from "leaflet";
-import "leaflet/dist/leaflet.css";
 
 interface UserLocation {
   latitude: number;
@@ -30,10 +28,10 @@ const TestGeolocation = () => {
 
 
   const geofenceAreas: GeofenceArea[] = [
-    { latitude: -6.9166349, longitude: 107.6615918, radius: 4, videoId: "DOOrIxw5xOw" },
-    { latitude: -6.9167522, longitude: 107.6614443, radius: 4, videoId: "36YnV9STBqc" },
-    { latitude: -6.9165868, longitude: 107.6613089, radius: 4, videoId: "lP26UCnoH9s" },
-    { latitude: -6.9164866, longitude: 107.6614578, radius: 4, videoId: "bk8WKwHDUNk" },
+     { latitude: -6.9166349, longitude: 107.6615918, radius: 4,  videoId: "DOOrIxw5xOw" },
+        { latitude: -6.9167522, longitude: 107.6614443, radius: 4,  videoId: "36YnV9STBqc" },
+        { latitude: -6.9165868, longitude: 107.6613089, radius: 4,  videoId: "lP26UCnoH9s" },
+        { latitude: -6.9164866, longitude: 107.6614578, radius: 4, videoId: "bk8WKwHDUNk" },
   ];
 
   const getUserLocation = () => {
@@ -156,42 +154,9 @@ const TestGeolocation = () => {
     setPlayer(event.target);
   };
 
-
-  // Initial map setup
-  useEffect(() => {
-    const map = L.map("map").setView([-6.9166349, 107.6615918], 16);
-
-    // Add tile layer (you can use any tile provider)
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      attribution: "&copy; OpenStreetMap contributors"
-    }).addTo(map);
-
-    // Add marker for user location
-    if (userLocation) {
-      L.marker([userLocation.latitude, userLocation.longitude]).addTo(map);
-    }
-
-    // Add geofence areas
-    geofenceAreas.forEach((area) => {
-      // For simplicity, you can use circle for geofence area
-      L.circle([area.latitude, area.longitude], {
-        color: "red",
-        fillColor: "#f03",
-        fillOpacity: 0.2,
-        radius: area.radius * 1000 // Convert to meters
-      }).addTo(map);
-    });
-
-    return () => {
-      map.remove(); // Clean up when component unmounts
-    };
-  }, [userLocation, geofenceAreas]);
-
-
   return (
     <>
       <h1>Geolocation App</h1>
-      <div id="map" style={{ height: "400px" }}></div>
       <div className="w-full flex justify-center items-center gap-4">
         <button className="border border-slate-300 text-sm font-semibold px-4 py-2 rounded-md" onClick={getUserLocation}>Get User Location</button>
         <button className="border border-slate-300 text-sm font-semibold px-4 py-2 rounded-md" onClick={watchUserLocation}>Start Watching User Location</button>
@@ -219,7 +184,7 @@ const TestGeolocation = () => {
         ))}
       </div>
 
-
+      
     </>
   );
 
