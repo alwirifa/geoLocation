ebf70bf51ed811adff2ae6f3236129ba08f83108
Mochamad ilham alwi rifa <95983472+alwirifa@users.noreply.github.com>
2024-03-22 16:36:16 +0700

    tes artha 5

--- app/page.tsx
+++ app/page.tsx
@@ -32,10 +32,10 @@ const Page = () => {
 
   const geofenceAreas: GeofenceArea[] = [
     // //  artha
-    { latitude: -6.925384166999025, longitude: 107.66488749238019, radius: 6, videoId: "DOOrIxw5xOw" },
-    { latitude: -6.9256437742903625, longitude: 107.66496997030328, radius: 6, videoId: "36YnV9STBqc" },
-    { latitude: -6.925479356355771, longitude: 107.6645709917322, radius: 6, videoId: "lP26UCnoH9s" },
-    { latitude: -6.925742957551122, longitude: 107.66467023346081, radius: 6, videoId: "bk8WKwHDUNk" },
+    { latitude: -6.925368719382597, longitude: 107.6648914323083, radius: 6, videoId: "lP26UCnoH9s" },
+    { latitude: -6.925666016230372, longitude: 107.66497922370358, radius: 6, videoId: "DOOrIxw5xOw" },
+    { latitude: -6.925464304205161, longitude: 107.6645636305663, radius: 6, videoId: "bk8WKwHDUNk" },
+    { latitude: -6.925770034941376, longitude: 107.66466558186403, radius: 6, videoId: "36YnV9STBqc" },
 
     // // gasmin
     // { latitude: -6.9166349, longitude: 107.6615918, radius: 4, videoId: "DOOrIxw5xOw" },
@@ -216,8 +216,18 @@ const Page = () => {
               ) : (
                 <p className="mt-4 font-semibold">You are not inside any geofence area</p>
               )}
+
+{userLocation && (
+        <div>
+          <h2>User Location</h2>
+          <p>Latitude: {userLocation.latitude}</p>
+          <p>Longitude: {userLocation.longitude}</p>
+          <p>Accuracy: {userLocation.accuracy} meters</p>
+          <p>Speed: {userLocation.speed} meters/second</p>
+        </div>
+      )}
             </div>
-            <div className="hidden">
+            <div className="flex">
               {geofenceAreas.map((area) => (
                 <YouTube
                   key={area.videoId}
