ee48be081cea0f447c502e41133f90ea732d400f
Mochamad ilham alwi rifa <95983472+alwirifa@users.noreply.github.com>
2024-03-25 10:58:18 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -39,10 +39,16 @@ const Page = () => {
 
     // // gasmin
     { latitude: -6.9166387, longitude: 107.6615271, radius: 4, videoId: "DOOrIxw5xOw" },
-    { latitude: -6.9167322, longitude: 107.6613635, radius: 4, videoId: "36YnV9STBqc" },
-    { latitude: -6.9167383, longitude: 107.6615612, radius: 4, videoId: "lP26UCnoH9s" },
-    { latitude: -6.9167883, longitude: 107.661473, radius: 4, videoId: "bk8WKwHDUNk" },
+    
+    
+ { latitude: -6.9167883, longitude: 107.661473, radius: 4, videoId: "bk8WKwHDUNk" },
 
+{ latitude: -6.9167322, longitude: 107.6613635, radius: 4, videoId: "36YnV9STBqc" },
+
+   
+
+
+{ latitude: -6.9167383, longitude: 107.6615612, radius: 4, videoId: "lP26UCnoH9s" },
   ]
 
 
