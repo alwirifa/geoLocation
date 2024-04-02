efb345d2cb6d62c41085672e40a46b180ddb7218
Mochamad ilham alwi rifa <95983472+alwirifa@users.noreply.github.com>
2024-04-02 13:53:15 +0700

    music tes

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
-"use client"
-
 import React, { useEffect, useState } from 'react';
 import Navbar from './components/Navbar';
 import YouTube from "react-youtube";
 
 import { enData, idData } from './data';
 import { useGlobalContext } from './context/store';
-
-
 
 interface UserLocation {
   latitude: number;
@@ -16,160 +12,19 @@ interface UserLocation {
   speed: number | null;
 }
 
-interface GeofenceArea {
-  latitude: number;
-  longitude: number;
-  radius: number;
-  videoId: string;
-}
-
 const Page = () => {
   const [experienceStarted, setExperienceStarted] = useState(false);
-  const [userLocation, setUserLocation] = useState<UserLocation | null>(null);
-  const [watchId, setWatchId] = useState<number | null>(null);
-  const [currentVideoId, setCurrentVideoId] = useState<string | null>(null);
-  const [isPlaying, setIsPlaying] = useState(false);
-  const [player, setPlayer] = useState<any>(null);
-  const [currentAreaIndex, setCurrentAreaIndex] = useState<number | null>(null);
-
-  const { language, toggleLanguage } = useGlobalContext();
+  const [currentVideoId, setCurrentVideoId] = useState<string>("yNKvkPJl-tg"); // Set the default video ID here
+  const { language } = useGlobalContext();
   const data = language === 'en' ? enData : idData;
 
-
-  const geofenceAreas: GeofenceArea[] = [
-
-    // { latitude: -6.925368719382597, longitude: 107.6648914323083, radius: 6, videoId: "lP26UCnoH9s" },
-    // { latitude: -6.925666016230372, longitude: 107.66497922370358, radius: 6, videoId: "DOOrIxw5xOw" },
-    // { latitude: -6.925464304205161, longitude: 107.6645636305663, radius: 6, videoId: "bk8WKwHDUNk" },
-    // { latitude: -6.925770034941376, longitude: 107.66466558186403, radius: 6, videoId: "36YnV9STBqc" },
-
-
-    // // gasmin
-    // { latitude: -6.9166387, longitude: 107.6615271, radius: 4, videoId: "DOOrIxw5xOw" },
-
-    // { latitude: -6.9167608, longitude: 107.6616099, radius: 4, videoId: "XnUNOaxw6bs" },
-
-    // { latitude: -6.9167322, longitude: 107.6613635, radius: 4, videoId: "36YnV9STBqc" },
-
-    // { latitude: -6.9168766, longitude: 107.6614897, radius: 4, videoId: "bk8WKwHDUNk" },
-
-    // jkt
-
-
-    
-    { latitude: -6.2223542, longitude: 106.806881, radius: 20, videoId: "lP26UCnoH9s" },
-    { latitude: -6.2220232, longitude: 106.8068387, radius: 20, videoId: "WkBX4N79r4w" },
-    { latitude: -6.2216925, longitude: 106.8064602, radius: 25, videoId: "bk8WKwHDUNk" },
-    { latitude: -6.221902133889262, longitude: 106.80623434081818, radius: 20, videoId: "36YnV9STBqc" },
-    { latitude: -6.2222515, longitude: 106.8060507, radius: 20, videoId: "RP0vhIfNOQQ" },
-    { latitude: -6.2216579, longitude: 106.806822, radius: 20, videoId: "ku5VKha1VB8" },
-  ]
-
-  const watchUserLocation = () => {
-    setExperienceStarted(true);
-  
-    if (navigator.geolocation) {
-      const id = navigator.geolocation.watchPosition(
-        (position) => {
-          const { latitude, longitude, accuracy, speed } = position.coords;
-          setUserLocation({ latitude, longitude, accuracy, speed });
-  
-          let isInsideAnyGeofence = false;
-          let areaIndex = null;
-  
-          geofenceAreas.forEach((area, index) => {
-            const distance = calculateDistance(latitude, longitude, area.latitude, area.longitude);
-            if (distance <= area.radius) {
-              isInsideAnyGeofence = true;
-              areaIndex = index;
-              if (currentVideoId !== area.videoId) {
-                setCurrentVideoId(area.videoId);
-              }
-            }
-          });
-  
-          if (isInsideAnyGeofence) {
-            setIsPlaying(true);
-            setCurrentAreaIndex(areaIndex);
-            // Pause the specified video if it's currently playing
-            if (currentVideoId === "yNKvkPJl-tg") {
-              setIsPlaying(false);
-            }
-          } else {
-            // If the user is outside of any geofence area, play the specified video
-            setCurrentVideoId("yNKvkPJl-tg");
-            setIsPlaying(true);
-            setCurrentAreaIndex(null);
-          }
-        },
-        (error) => {
-          console.error("Error watching user location: ", error);
-        },
-        {
-          enableHighAccuracy: true,
-          maximumAge: 0,
-          timeout: 5000,
-        }
-      );
-      setWatchId(id);
-    } else {
-      console.log("Geolocation is not supported by this browser");
-    }
-  };
-  
-  
-
-  const stopWatchUserLocation = () => {
-    if (watchId !== null) {
-      navigator.geolocation.clearWatch(watchId);
-      setWatchId(null);
-    }
-
-    setExperienceStarted(false)
-  };
-
   useEffect(() => {
-    return () => {
-      stopWatchUserLocation();
-    };
+    setExperienceStarted(false);
   }, []);
 
-
-  const onReady = (event: any) => {
-    setPlayer(event.target);
-  };
-
-  useEffect(() => {
-    if (player && currentVideoId) {
-      player.loadVideoById(currentVideoId);
-      if (isPlaying) {
-        player.playVideo();
-      } else {
-        player.pauseVideo();
-      }
-    }
-  }, [player, currentVideoId, isPlaying]);
-
-
-  const calculateDistance = (lat1: number, lon1: number, lat2: number, lon2: number): number => {
-    const R = 6371; // Earth's radius in kilometers
-    const dLat = deg2rad(lat2 - lat1);
-    const dLon = deg2rad(lon2 - lon1);
-    const a =
-      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-      Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    const distance = R * c;  // in km
-    return distance * 1000; // Convert to meters
-  };
-
-  const deg2rad = (deg: number): number => {
-    return deg * (Math.PI / 180);
-  };
-
   return (
-    <div className='h-[100svh] w-full relative '>
-      <div className='bg-background h-[100svh] w-full bg-cover bg-center'>
+    <div className='h-screen w-full relative'>
+      <div className='bg-background h-full w-full bg-cover bg-center'>
 
         <div className='absolute top-0 flex justify-between w-full p-8'>
           <div>
@@ -182,7 +37,7 @@ const Page = () => {
         </div>
 
         {!experienceStarted ? (
-          <div className={`flex flex-col items-center justify-center w-full pt-[16svh]`}>
+          <div className='flex flex-col items-center justify-center w-full pt-[16svh]'>
 
             <div className='flex flex-col justify-center items-center gap-1'>
               <p className='text-lg text-purple font-semibold'>HERE, NOWHERE HEAR</p>
@@ -201,10 +56,6 @@ const Page = () => {
         ) : (
 
           <div className="flex flex-col gap-4 items-center  pt-[16svh]">
-            {/* <div className="relative h-[250px] w-[250px] border-2 border-black">
-             
-            </div> */}
-
 
             <div className='flex flex-col justify-center items-center gap-1'>
               <p className='text-lg text-purple font-semibold'>HERE, NOWHERE HEAR</p>
@@ -216,56 +67,11 @@ const Page = () => {
               </div>
               <img src="/images/map.png" alt="" className='h-auto w-[300px]' />
               <div className='bg-purple  flex justify-between items-center p-2 px-4 absolute bottom-0 w-full'>
-                {currentAreaIndex !== null ? (
-                  <p className="text-white font-semibold">AUDIO {currentAreaIndex}</p>
-                ) : (
-                  <p className="text-white font-semibold">OUT OF AREA</p>
-                )}
+                <p className="text-white font-semibold">OUT OF AREA</p>
                 <div>
                   <img src="/images/audio.png" alt="" className='h-6 w-6' />
                 </div>
               </div>
-
-              <div className='absolute bottom-[134px] right-16 p-4 border-2 border-green-500 flex justify-center items-center'>
-                <div className={`bg-red-500 h-4 w-4 rounded-full animate-ping absolute ${currentAreaIndex === 0 ? 'visible' : 'hidden'}`} />
-              </div>
-              <div className='absolute top-36 right-24 p-4 border-2 border-green-500 flex justify-center items-center'>
-                <div className={`bg-red-500 h-4 w-4 rounded-full animate-ping absolute ${currentAreaIndex === 1 ? 'visible' : 'hidden'}`} />
-              </div>
-              <div className='absolute top-20 left-32 p-4 border-2 border-green-500 flex justify-center items-center'>
-                <div className={`bg-red-500 h-4 w-4 rounded-full animate-ping absolute ${currentAreaIndex === 2 ? 'visible' : 'hidden'}`} />
-              </div>
-              <div className='absolute top-32 left-16 p-4 border-2 border-green-500 flex justify-center items-center'>
-                <div className={`bg-red-500 h-4 w-4 rounded-full animate-ping absolute ${currentAreaIndex === 3 ? 'visible' : 'hidden'}`} />
-              </div>
-              
-              <div className='absolute bottom-40 left-4 p-4 border-2 border-green-500 flex justify-center items-center'>
-                <div className={`bg-red-500 h-4 w-4 rounded-full animate-ping absolute ${currentAreaIndex === 4 ? 'visible' : 'hidden'}`} />
-              </div>
-              
-              <div className='absolute top-16 right-8 p-4 border-2 border-green-500 flex justify-center items-center'>
-                <div className={`bg-red-500 h-4 w-4 rounded-full animate-ping absolute ${currentAreaIndex === 5 ? 'visible' : 'hidden'}`} />
-              </div>
-            </div>
-
-
-            
-            {/* {currentAreaIndex !== null ? (
-              <p className="mt-4 font-semibold">You are currently inside geofence area </p>
-            ) : (
-              <p className="mt-4 font-semibold">You are not inside any geofence area</p>
-            )} */}
-
-
-            <div className="hidden">
-              {geofenceAreas.map((area) => (
-                <YouTube
-                  key={area.videoId}
-                  videoId={area.videoId}
-                  onReady={onReady}
-                  opts={{ height: "100", width: "100", controls: 0, autoplay: 0 }}
-                />
-              ))}
             </div>
           </div>
 
@@ -275,17 +81,11 @@ const Page = () => {
         {experienceStarted ? (
 
           <div className='absolute w-full bottom-[14svh] flex flex-col gap-4 justify-center items-center'>
-            {/* <img src="/images/headphones.png" alt="" className='h-24 w-24' />
-
-            <button className='border border-purple text-purple font-semibold px-6 py-2 rounded-full max-w-max' onClick={stopWatchUserLocation}>
-              BERHENTI MENDENGARKAN
-            </button> */}
           </div>
         ) : (
           <div className='absolute w-full bottom-[14svh] flex flex-col gap-4 justify-center items-center'>
             <img src="/images/headphones.png" alt="" className='h-24 w-24' />
-
-            <button className='border border-purple text-purple font-semibold px-6 py-2 rounded-full max-w-max' onClick={watchUserLocation}>
+            <button className='border border-purple text-purple font-semibold px-6 py-2 rounded-full max-w-max' onClick={() => setExperienceStarted(true)}>
               {data.button}
             </button>
           </div>
@@ -296,6 +96,10 @@ const Page = () => {
         </div>
 
       </div>
+      <YouTube
+        videoId={currentVideoId}
+        opts={{ height: "100", width: "100", controls: 0, autoplay: 1 }}
+      />
     </div>
   );
 };
