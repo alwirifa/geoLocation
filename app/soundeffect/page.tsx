e4ccbe7830f95257a050758c353663bd7f09e6c0
Mochamad ilham alwi rifa <95983472+alwirifa@users.noreply.github.com>
2024-04-02 11:24:26 +0700

    tesjkt2

--- app/soundeffect/page.tsx
+++ app/soundeffect/page.tsx
@@ -1,76 +1,76 @@
-// "use client"
+"use client"
 
-// import React from 'react';
-// import { XSound, X } from 'xsound'; 
+import React from 'react';
+import { XSound, X } from 'xsound'; 
 
-// const AudioButton: React.FC = () => {
-//   const playAudio = () => {
-//     // path to  audio file
-//     const audioPath = '/music/habibi.mp3';
+const AudioButton: React.FC = () => {
+  const playAudio = () => {
+    // path to  audio file
+    const audioPath = '/music/habibi.mp3';
 
-//     // Create an instance of AudioContext
-//     const context = XSound.get();
+    // Create an instance of AudioContext
+    const context = XSound.get();
 
-//     // Create an audio buffer source
-//     const source = context.createBufferSource();
+    // Create an audio buffer source
+    const source = context.createBufferSource();
 
-//     // EFFECT OVERDRIVE
-//     fetch(audioPath)
-//       .then((response) => response.arrayBuffer())
-//       .then((data) => context.decodeAudioData(data))
-//       .then((buffer) => {
-//         source.buffer = buffer;
+    // EFFECT OVERDRIVE
+    fetch(audioPath)
+      .then((response) => response.arrayBuffer())
+      .then((data) => context.decodeAudioData(data))
+      .then((buffer) => {
+        source.buffer = buffer;
 
-//         const overdrive = new X.OverDrive(context);
+        const overdrive = new X.OverDrive(context);
 
-//         source.connect(overdrive.INPUT);
+        source.connect(overdrive.INPUT);
 
-//         overdrive.OUTPUT.connect(context.destination);
+        overdrive.OUTPUT.connect(context.destination);
 
-//         overdrive.param({
-//           drive: 1, 
+        overdrive.param({
+          drive: 1, 
          
-//         });
+        });
 
-//         overdrive.activate();
+        overdrive.activate();
 
-//         source.start(0);
-//       })
+        source.start(0);
+      })
 
 
-//       // EFFECT PITCH SHIFTER
+      // EFFECT PITCH SHIFTER
 
-//       // fetch(audioPath)
-//       // .then((response) => response.arrayBuffer())
-//       // .then((data) => context.decodeAudioData(data))
-//       // .then((buffer) => {
-//       //   source.buffer = buffer;
+      // fetch(audioPath)
+      // .then((response) => response.arrayBuffer())
+      // .then((data) => context.decodeAudioData(data))
+      // .then((buffer) => {
+      //   source.buffer = buffer;
 
-//       //   const pitchShifter = new X.PitchShifter(context);
+      //   const pitchShifter = new X.PitchShifter(context);
 
-//       //   source.connect(pitchShifter.INPUT);
+      //   source.connect(pitchShifter.INPUT);
 
-//       //   pitchShifter.OUTPUT.connect(context.destination);
+      //   pitchShifter.OUTPUT.connect(context.destination);
 
-//       //   pitchShifter.param({
-//       //     state: true,
-//       //     pitch: 2, 
-//       //   });
+      //   pitchShifter.param({
+      //     state: true,
+      //     pitch: 2, 
+      //   });
 
-//       //   pitchShifter.activate();
+      //   pitchShifter.activate();
 
-//       //   source.start(0);
-//       // })
+      //   source.start(0);
+      // })
 
       
-//       .catch((error) => console.error('Error loading audio:', error));
-//   };
+      .catch((error) => console.error('Error loading audio:', error));
+  };
 
   
 
-//   return (
-//     <button onClick={playAudio}>Play Audio with OverDrive</button>
-//   );
-// };
+  return (
+    <button onClick={playAudio}>Play Audio with OverDrive</button>
+  );
+};
 
-// export default AudioButton;
+export default AudioButton;
