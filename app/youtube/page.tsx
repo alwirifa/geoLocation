89a91b83ea61cc52443553d9bb63aafd8a905ecb
Mochamad ilham alwi rifa <95983472+alwirifa@users.noreply.github.com>
2024-04-02 11:21:37 +0700

    tes jkt

--- app/youtube/page.tsx
+++ app/youtube/page.tsx
@@ -1,11 +1,43 @@
-import React from 'react'
+// import React from 'react';
+// import { XSound, X } from 'xsound';
+// import YouTube from 'react-youtube';
 
-type Props = {}
+// const VideoPlayer: React.FC = () => {
+//   const playVideo = () => {
+//     // URL dari video YouTube
+//     const videoId = 'DOOrIxw5xOw';
 
-const page = (props: Props) => {
-  return (
-    <div>page</div>
-  )
-}
+//     // Create an instance of AudioContext
+//     const context = XSound.get();
 
-export default page
+//     // EFFECT OVERDRIVE
+//     const overdrive = new X.OverDrive(context);
+//     overdrive.param({
+//       drive: 1
+//     });
+//     overdrive.activate();
+
+//     // EFFECT PITCH SHIFTER
+//     const pitchShifter = new X.PitchShifter(context);
+//     pitchShifter.param({
+//       state: true,
+//       pitch: 2
+//     });
+//     pitchShifter.activate();
+
+//     // Start playing the video
+//     XSound.play({
+//       src: `youtube:${videoId}`,
+//       effect: [overdrive, pitchShifter]
+//     });
+//   };
+
+//   return (
+//     <div>
+//       <button onClick={playVideo}>Play Video with Effects</button>
+//       <YouTube videoId="DOOrIxw5xOw" />
+//     </div>
+//   );
+// };
+
+// export default VideoPlayer;
