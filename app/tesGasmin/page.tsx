9d52dce9384757c20870dd26f0b9c47ea8c3255d
Mochamad ilham alwi rifa <95983472+alwirifa@users.noreply.github.com>
2024-04-03 11:39:23 +0700

    new

--- app/tesGasmin/page.tsx
+++ app/tesGasmin/page.tsx
@@ -27,43 +27,69 @@ const Page = () => {
   const [player, setPlayer] = useState<any>(null);
   const [currentAreaIndex, setCurrentAreaIndex] = useState<number | null>(null);
 
+
   const geofenceAreas: GeofenceArea[] = [
-    { latitude: -6.9167322, longitude: 107.6613635, radius: 10, videoId: "36YnV9STBqc" },
-    { latitude: -6.9166387, longitude: 107.6615271, radius: 10, videoId: "DOOrIxw5xOw" },
-    { latitude: -6.9167608, longitude: 107.6616099, radius: 10, videoId: "XnUNOaxw6bs" },
-    { latitude: -6.9168766, longitude: 107.6614897, radius: 10, videoId: "bk8WKwHDUNk" },
-  ];
+
+    // { latitude: -6.925368719382597, longitude: 107.6648914323083, radius: 6, videoId: "lP26UCnoH9s" },
+    // { latitude: -6.925666016230372, longitude: 107.66497922370358, radius: 6, videoId: "DOOrIxw5xOw" },
+    // { latitude: -6.925464304205161, longitude: 107.6645636305663, radius: 6, videoId: "bk8WKwHDUNk" },
+    // { latitude: -6.925770034941376, longitude: 107.66466558186403, radius: 6, videoId: "36YnV9STBqc" },
+
+
+    // // gasmin
+    // { latitude: -6.9166387, longitude: 107.6615271, radius: 4, videoId: "DOOrIxw5xOw" },
+
+    // { latitude: -6.9167608, longitude: 107.6616099, radius: 4, videoId: "XnUNOaxw6bs" },
+
+    // { latitude: -6.9167322, longitude: 107.6613635, radius: 4, videoId: "36YnV9STBqc" },
+
+    // { latitude: -6.9168766, longitude: 107.6614897, radius: 4, videoId: "bk8WKwHDUNk" },
+
+    // jkt
+
+
+    
+    { latitude: -6.2223542, longitude: 106.806881, radius: 20, videoId: "lP26UCnoH9s" },
+    { latitude: -6.2220232, longitude: 106.8068387, radius: 20, videoId: "WkBX4N79r4w" },
+    { latitude: -6.2216925, longitude: 106.8064602, radius: 25, videoId: "bk8WKwHDUNk" },
+    { latitude: -6.221902133889262, longitude: 106.80623434081818, radius: 20, videoId: "36YnV9STBqc" },
+    { latitude: -6.2222515, longitude: 106.8060507, radius: 20, videoId: "RP0vhIfNOQQ" },
+    { latitude: -6.2216579, longitude: 106.806822, radius: 20, videoId: "ku5VKha1VB8" },
+  ]
 
   const watchUserLocation = () => {
     setExperienceStarted(true);
-
+  
     if (navigator.geolocation) {
       const id = navigator.geolocation.watchPosition(
         (position) => {
           const { latitude, longitude, accuracy, speed } = position.coords;
           setUserLocation({ latitude, longitude, accuracy, speed });
-
+  
           let isInsideAnyGeofence = false;
-          let areaIndex: number | null = null;
-
+          let areaIndex = null;
+          let videoToPlay = "yNKvkPJl-tg"; // Default video to play if not in any geofence area
+  
           geofenceAreas.forEach((area, index) => {
             const distance = calculateDistance(latitude, longitude, area.latitude, area.longitude);
             if (distance <= area.radius) {
               isInsideAnyGeofence = true;
               areaIndex = index;
-              if (currentVideoId !== area.videoId) {
-                setCurrentVideoId(area.videoId);
-              }
+              videoToPlay = area.videoId; // Set the video to play if inside a geofence area
             }
           });
-
-          if (isInsideAnyGeofence) {
-            setIsPlaying(true);
-            setCurrentAreaIndex(areaIndex);
-          } else {
+  
+          setIsPlaying(true); // Assume video should play by default
+  
+          if (currentVideoId !== videoToPlay) {
+            setCurrentVideoId(videoToPlay); // Set the video to play if it's different from the current one
+          }
+  
+          setCurrentAreaIndex(isInsideAnyGeofence ? areaIndex : null); // Set the current area index
+  
+          // Pause the specified video if it's currently playing and not in any geofence area
+          if (!isInsideAnyGeofence && currentVideoId === "yNKvkPJl-tg") {
             setIsPlaying(false);
-            setCurrentAreaIndex(null);
-            setCurrentVideoId("M94RrCFqaw4"); // Mengatur video yang akan diputar saat di luar area
           }
         },
         (error) => {
@@ -80,6 +106,8 @@ const Page = () => {
       console.log("Geolocation is not supported by this browser");
     }
   };
+  
+  
 
   const stopWatchUserLocation = () => {
     if (watchId !== null) {
@@ -87,7 +115,7 @@ const Page = () => {
       setWatchId(null);
     }
 
-    setExperienceStarted(false);
+    setExperienceStarted(false)
   };
 
   useEffect(() => {
@@ -95,6 +123,7 @@ const Page = () => {
       stopWatchUserLocation();
     };
   }, []);
+
 
   const onReady = (event: any) => {
     setPlayer(event.target);
@@ -111,6 +140,7 @@ const Page = () => {
     }
   }, [player, currentVideoId, isPlaying]);
 
+
   const calculateDistance = (lat1: number, lon1: number, lat2: number, lon2: number): number => {
     const R = 6371; // Earth's radius in kilometers
     const dLat = deg2rad(lat2 - lat1);
@@ -119,13 +149,14 @@ const Page = () => {
       Math.sin(dLat / 2) * Math.sin(dLat / 2) +
       Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    const distance = R * c; // in km
+    const distance = R * c;  // in km
     return distance * 1000; // Convert to meters
   };
 
   const deg2rad = (deg: number): number => {
     return deg * (Math.PI / 180);
   };
+
 
   return (
     <div className='h-[100svh] w-full relative '>
