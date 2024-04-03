aabd1adf1eedd2e673c62f4eccf37a793c6f617c
Mochamad ilham alwi rifa <95983472+alwirifa@users.noreply.github.com>
2024-04-03 14:17:42 +0700

    tes

--- app/tesGasmin/page.tsx
+++ app/tesGasmin/page.tsx
@@ -1,8 +1,11 @@
-"use client";
+"use client"
 
 import React, { useEffect, useState } from 'react';
 import Navbar from '../components/Navbar';
 import YouTube from "react-youtube";
+import { CircleSpinner } from 'react-spinner-overlay';
+
+
 
 interface UserLocation {
   latitude: number;
@@ -15,27 +18,49 @@ interface GeofenceArea {
   latitude: number;
   longitude: number;
   radius: number;
-  media: string; // Mengganti nama properti videoId menjadi media dan tipenya string
+  videoId: string;
 }
 
 const Page = () => {
   const [experienceStarted, setExperienceStarted] = useState(false);
   const [userLocation, setUserLocation] = useState<UserLocation | null>(null);
   const [watchId, setWatchId] = useState<number | null>(null);
-  const [currentMedia, setCurrentMedia] = useState<string | null>(null); // Mengganti currentVideoId menjadi currentMedia
+  const [currentVideoId, setCurrentVideoId] = useState<string | null>(null);
   const [isPlaying, setIsPlaying] = useState(false);
   const [player, setPlayer] = useState<any>(null);
   const [currentAreaIndex, setCurrentAreaIndex] = useState<number | null>(null);
+  const [isLoading, setIsLoading] = useState(true); // State to manage loading modal
+
 
   const geofenceAreas: GeofenceArea[] = [
-    { latitude: -6.9167322, longitude: 107.6613635, radius: 10, media: "/music/habibi.mp3" }, // Mengganti videoId menjadi media dan memberikan alamat file MP3
-    { latitude: -6.9166387, longitude: 107.6615271, radius: 10, media: "36YnV9STBqc" },
-    { latitude: -6.9167608, longitude: 107.6616099, radius: 10, media: "DOOrIxw5xOw" },
-    { latitude: -6.9168766, longitude: 107.6614897, radius: 10, media: "XnUNOaxw6bs" },
-  ];
+
+    // { latitude: -6.925368719382597, longitude: 107.6648914323083, radius: 6, videoId: "lP26UCnoH9s" },
+    // { latitude: -6.925666016230372, longitude: 107.66497922370358, radius: 6, videoId: "DOOrIxw5xOw" },
+    // { latitude: -6.925464304205161, longitude: 107.6645636305663, radius: 6, videoId: "bk8WKwHDUNk" },
+    // { latitude: -6.925770034941376, longitude: 107.66466558186403, radius: 6, videoId: "36YnV9STBqc" },
+
+
+    // // gasmin
+    { latitude: -6.9166387, longitude: 107.6615271, radius: 4, videoId: "DOOrIxw5xOw" },
+
+    { latitude: -6.9167608, longitude: 107.6616099, radius: 4, videoId: "XnUNOaxw6bs" },
+
+    { latitude: -6.9167322, longitude: 107.6613635, radius: 4, videoId: "36YnV9STBqc" },
+
+    { latitude: -6.9168766, longitude: 107.6614897, radius: 4, videoId: "bk8WKwHDUNk" },
+
+    // jkt
+    // { latitude: -6.2223542, longitude: 106.806881, radius: 20, videoId: "lP26UCnoH9s" },
+    // { latitude: -6.2220232, longitude: 106.8068387, radius: 20, videoId: "WkBX4N79r4w" },
+    // { latitude: -6.2216925, longitude: 106.8064602, radius: 25, videoId: "bk8WKwHDUNk" },
+    // { latitude: -6.221902133889262, longitude: 106.80623434081818, radius: 20, videoId: "36YnV9STBqc" },
+    // { latitude: -6.2222515, longitude: 106.8060507, radius: 20, videoId: "RP0vhIfNOQQ" },
+    // { latitude: -6.2216579, longitude: 106.806822, radius: 20, videoId: "ku5VKha1VB8" },
+  ]
 
   const watchUserLocation = () => {
     setExperienceStarted(true);
+    setIsLoading(true);
 
     if (navigator.geolocation) {
       const id = navigator.geolocation.watchPosition(
@@ -44,26 +69,29 @@ const Page = () => {
           setUserLocation({ latitude, longitude, accuracy, speed });
 
           let isInsideAnyGeofence = false;
-          let areaIndex: number | null = null;
+          let areaIndex = null;
+          let videoToPlay = "yNKvkPJl-tg"; // Default video to play if not in any geofence area
 
           geofenceAreas.forEach((area, index) => {
             const distance = calculateDistance(latitude, longitude, area.latitude, area.longitude);
             if (distance <= area.radius) {
               isInsideAnyGeofence = true;
               areaIndex = index;
-              if (currentMedia !== area.media) { // Mengganti currentVideoId menjadi currentMedia
-                setCurrentMedia(area.media);
-              }
+              videoToPlay = area.videoId; // Set the video to play if inside a geofence area
             }
           });
 
-          if (isInsideAnyGeofence) {
-            setIsPlaying(true);
-            setCurrentAreaIndex(areaIndex);
-          } else {
+          setIsPlaying(true); // Assume video should play by default
+
+          if (currentVideoId !== videoToPlay) {
+            setCurrentVideoId(videoToPlay); // Set the video to play if it's different from the current one
+          }
+
+          setCurrentAreaIndex(isInsideAnyGeofence ? areaIndex : null); // Set the current area index
+
+          // Pause the specified video if it's currently playing and not in any geofence area
+          if (!isInsideAnyGeofence && currentVideoId === "yNKvkPJl-tg") {
             setIsPlaying(false);
-            setCurrentAreaIndex(null);
-            setCurrentMedia("/music/habibi.mp3"); // Mengatur file MP3 yang akan diputar saat di luar area
           }
         },
         (error) => {
@@ -81,13 +109,15 @@ const Page = () => {
     }
   };
 
+
+
   const stopWatchUserLocation = () => {
     if (watchId !== null) {
       navigator.geolocation.clearWatch(watchId);
       setWatchId(null);
     }
 
-    setExperienceStarted(false);
+    setExperienceStarted(false)
   };
 
   useEffect(() => {
@@ -96,27 +126,22 @@ const Page = () => {
     };
   }, []);
 
+
   const onReady = (event: any) => {
     setPlayer(event.target);
   };
 
   useEffect(() => {
-    if (player && currentMedia) { // Mengganti currentVideoId menjadi currentMedia
-      if (currentMedia.endsWith(".mp3")) { // Memeriksa apakah media berupa file MP3
-        player.loadMedia({
-          contentId: currentMedia,
-          contentType: 'audio/mp3',
-        });
-      } else {
-        player.loadVideoById(currentMedia);
-      }
+    if (player && currentVideoId) {
+      player.loadVideoById(currentVideoId);
       if (isPlaying) {
-        player.playMedia(); // Mengganti playVideo menjadi playMedia
+        player.playVideo();
       } else {
-        player.pauseMedia(); // Mengganti pauseVideo menjadi pauseMedia
+        player.pauseVideo();
       }
+      setIsLoading(false); // Hide loading modal after video is ready to play
     }
-  }, [player, currentMedia, isPlaying]);
+  }, [player, currentVideoId, isPlaying]);
 
   const calculateDistance = (lat1: number, lon1: number, lat2: number, lon2: number): number => {
     const R = 6371; // Earth's radius in kilometers
@@ -126,13 +151,14 @@ const Page = () => {
       Math.sin(dLat / 2) * Math.sin(dLat / 2) +
       Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    const distance = R * c; // in km
+    const distance = R * c;  // in km
     return distance * 1000; // Convert to meters
   };
 
   const deg2rad = (deg: number): number => {
     return deg * (Math.PI / 180);
   };
+
 
   return (
     <div className='h-[100svh] w-full relative '>
@@ -192,17 +218,12 @@ const Page = () => {
 
             <div className="flex">
               {geofenceAreas.map((area) => (
-                <React.Fragment key={area.media}>
-                  {area.media.endsWith(".mp3") ? ( // Memeriksa apakah media berupa file MP3
-                    <audio src={area.media} controls={false} autoPlay={false} />
-                  ) : (
-                    <YouTube
-                      videoId={area.media}
-                      onReady={onReady}
-                      opts={{ height: "100", width: "100", controls: 0, autoplay: 0 }}
-                    />
-                  )}
-                </React.Fragment>
+                <YouTube
+                  key={area.videoId}
+                  videoId={area.videoId}
+                  onReady={onReady}
+                  opts={{ height: "100", width: "100", controls: 0, autoplay: 0 }}
+                />
               ))}
             </div>
           </div>
@@ -211,13 +232,26 @@ const Page = () => {
         }
 
         {experienceStarted ? (
+          <div>
 
-          <div className='absolute w-full bottom-[14svh] flex flex-col gap-4 justify-center items-center'>
-            <img src="/images/headphones.png" alt="" className='h-24 w-24' />
+            <div className='absolute w-full bottom-[14svh] flex flex-col gap-4 justify-center items-center'>
+              <img src="/images/headphones.png" alt="" className='h-24 w-24' />
 
-            <button className='border border-purple text-purple font-semibold px-6 py-2 rounded-full max-w-max' onClick={stopWatchUserLocation}>
-              STOP LISTENING
-            </button>
+              <button className='border border-purple text-purple font-semibold px-6 py-2 rounded-full max-w-max' onClick={stopWatchUserLocation}>
+                STOP LISTENING
+              </button>
+              {isLoading && (
+                // Modal loading
+                <div className='fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50'>
+                  <CircleSpinner
+                    color='#FFF'
+                    outerBorderOpacity={0.5}
+                    outerBorderWidth={3}
+                    innerBorderWidth={3}
+                  />
+                </div>
+              )}
+            </div>
           </div>
         ) : (
           <div className='absolute w-full bottom-[14svh] flex flex-col gap-4 justify-center items-center'>
@@ -228,6 +262,8 @@ const Page = () => {
             </button>
           </div>
         )}
+
+
 
         <div className='absolute bottom-0 w-full'>
           <Navbar />
