d622ac31d965068742c1c5c1ad9cffb218550e3b
Mochamad ilham alwi rifa <95983472+alwirifa@users.noreply.github.com>
2024-04-04 11:02:01 +0700

    tes

--- app/tesGasmin/page.tsx
+++ app/tesGasmin/page.tsx
@@ -30,27 +30,33 @@ const CustomYouTubePlayer = () => {
   const [isLoading, setIsLoading] = useState(true);
   const playerRef = useRef<any>(null);
 
+
   const geofenceAreas: GeofenceArea[] = [
+
+    // // gasmin
     { latitude: -6.925590413992352, longitude: 107.6650155604546, radius: 10, videoId: "DOOrIxw5xOw" },
-    { latitude: -6.9167608, longitude: 107.6616099, radius: 4, videoId: "XnUNOaxw6bs" },
+     { latitude: -6.9167608, longitude: 107.6616099, radius: 4, videoId: "XnUNOaxw6bs" },
+
     { latitude: -6.9167322, longitude: 107.6613635, radius: 4, videoId: "36YnV9STBqc" },
+
     { latitude: -6.9168766, longitude: 107.6614897, radius: 4, videoId: "bk8WKwHDUNk" },
     { latitude: -6.5168766, longitude: 107.7614897, radius: 4, videoId: 'yNKvkPJl-tg' }
-  ];
+
+  ]
 
   const watchUserLocation = () => {
     setExperienceStarted(true);
     setIsLoading(true);
-  
+
     if (navigator.geolocation) {
       const id = navigator.geolocation.watchPosition(
         (position) => {
           const { latitude, longitude, accuracy, speed } = position.coords;
           setUserLocation({ latitude, longitude, accuracy, speed });
-  
+
           let isInsideAnyGeofence = false;
-          let areaIndex: number | null = null;
-  
+          let areaIndex = null;
+
           geofenceAreas.forEach((area, index) => {
             const distance = calculateDistance(latitude, longitude, area.latitude, area.longitude);
             if (distance <= area.radius) {
@@ -58,10 +64,12 @@ const CustomYouTubePlayer = () => {
               areaIndex = index;
             }
           });
-  
-          setCurrentAreaIndex(areaIndex);
-  
-          playVideo();  
+
+          setIsPlaying(true);
+
+
+          setCurrentAreaIndex(isInsideAnyGeofence ? areaIndex : null);
+
         },
         (error) => {
           console.error('Error watching user location: ', error);
@@ -77,7 +85,6 @@ const CustomYouTubePlayer = () => {
       console.log('Geolocation is not supported by this browser');
     }
   };
-  
 
   const stopWatchUserLocation = () => {
     if (watchId !== null) {
@@ -86,7 +93,7 @@ const CustomYouTubePlayer = () => {
     }
     setExperienceStarted(false);
     if (player) {
-      player.pauseVideo();
+      player.pauseVideo(); // Memeriksa apakah player telah diinisialisasi sebelum memanggil pauseVideo()
     }
   };
 
@@ -101,6 +108,7 @@ const CustomYouTubePlayer = () => {
   useEffect(() => {
     if (player && currentVideoId) {
       player.loadVideoById(currentVideoId);
+      player.playVideo(); // Tambahkan pemutaran video secara otomatis saat komponen dimount
       if (isPlaying) {
         player.playVideo();
       } else {
@@ -134,18 +142,16 @@ const CustomYouTubePlayer = () => {
 
   const playVideo = () => {
     watchUserLocation();
-    if (player && currentAreaIndex !== null) {
-      const selectedArea = geofenceAreas[currentAreaIndex];
-      setCurrentVideoId(selectedArea.videoId);
+    if (player && currentAreaIndex !== 4) { // Memeriksa apakah currentAreaIndex bukan 4 (indeks untuk video di luar area)
+      player.playVideo(); // Memanggil fungsi playVideo() dari objek player
     }
   };
-
 
   const opts = {
     height: '100',
     width: '100',
     playerVars: {
-      autoplay: 0,
+      autoplay: 0, // Atur ke 1 untuk autoplay
     },
   };
 
@@ -164,6 +170,7 @@ const CustomYouTubePlayer = () => {
         </div>
 
         <div className='absolute top-0'>
+
           {geofenceAreas.map((area) => (
             <YouTube
               key={area.videoId}
@@ -215,25 +222,49 @@ const CustomYouTubePlayer = () => {
                 </div>
               </div>
 
-              {currentAreaIndex !== null && currentAreaIndex <= 3 && [0, 1, 2, 3].map((index) => (
-                <div key={index} className={`absolute bottom-[134px] right-16 p-4 border-2 border-green-500 flex justify-center items-center`}>
-                  <div className={`bg-red-500 h-4 w-4 rounded-full animate-ping absolute ${currentAreaIndex === index ? 'visible' : 'hidden'}`} />
-                </div>
-              ))}
+              <div className='absolute bottom-[134px] right-16 p-4 border-2 border-green-500 flex justify-center items-center'>
+                <div className={`bg-red-500 h-4 w-4 rounded-full animate-ping absolute ${currentAreaIndex === 0 ? 'visible' : 'hidden'}`} />
+              </div>
+              <div className='absolute top-36 right-24 p-4 border-2 border-green-500 flex justify-center items-center'>
+                <div className={`bg-red-500 h-4 w-4 rounded-full animate-ping absolute ${currentAreaIndex === 1 ? 'visible' : 'hidden'}`} />
+              </div>
+              <div className='absolute top-20 left-32 p-4 border-2 border-green-500 flex justify-center items-center'>
+                <div className={`bg-red-500 h-4 w-4 rounded-full animate-ping absolute ${currentAreaIndex === 2 ? 'visible' : 'hidden'}`} />
+              </div>
+              <div className='absolute top-32 left-16 p-4 border-2 border-green-500 flex justify-center items-center'>
+                <div className={`bg-red-500 h-4 w-4 rounded-full animate-ping absolute ${currentAreaIndex === 3 ? 'visible' : 'hidden'}`} />
+              </div>
 
-              {currentAreaIndex === 4 && (
-                <div className={`absolute bottom-40 left-4 p-4 border-2 border-green-500 flex justify-center items-center`}>
-                  <div className={`bg-red-500 h-4 w-4 rounded-full animate-ping absolute`} />
-                </div>
-              )}
+              <div className='absolute bottom-40 left-4 p-4 border-2 border-green-500 flex justify-center items-center'>
+                <div className={`bg-red-500 h-4 w-4 rounded-full animate-ping absolute ${currentAreaIndex === 4 ? 'visible' : 'hidden'}`} />
+              </div>
 
-              {currentAreaIndex === 5 && (
-                <div className={`absolute top-16 right-8 p-4 border-2 border-green-500 flex justify-center items-center`}>
-                  <div className={`bg-red-500 h-4 w-4 rounded-full animate-ping absolute`} />
-                </div>
-              )}
+              <div className='absolute top-16 right-8 p-4 border-2 border-green-500 flex justify-center items-center'>
+                <div className={`bg-red-500 h-4 w-4 rounded-full animate-ping absolute ${currentAreaIndex === 5 ? 'visible' : 'hidden'}`} />
+              </div>
             </div>
+
+
+
+            {/* {currentAreaIndex !== null ? (
+              <p className="mt-4 font-semibold">You are currently inside geofence area </p>
+            ) : (
+              <p className="mt-4 font-semibold">You are not inside any geofence area</p>
+            )} */}
+            {/* <div className="flex">
+              {geofenceAreas.map((area) => (
+                <YouTube
+                  key={area.videoId}
+                  videoId={area.videoId}
+                  onReady={onReady}
+                  opts={{ height: "100", width: "100", controls: 0, autoplay: 0 }}
+                />
+              ))}
+            </div> */}
+
+
           </div>
+
         )
         }
 
@@ -247,6 +278,11 @@ const CustomYouTubePlayer = () => {
               >
                 STOP LISTENING
               </button>
+              {/* {isLoading && (
+                <div className='fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50'>
+                  <CircleSpinner color='#FFF' outerBorderOpacity={0.5} outerBorderWidth={3} innerBorderWidth={3} />
+                </div>
+              )} */}
             </div>
           </div>
         ) : (
@@ -260,6 +296,7 @@ const CustomYouTubePlayer = () => {
             </button>
           </div>
         )}
+
 
         <div className='absolute bottom-0 w-full'>
           <Navbar />
