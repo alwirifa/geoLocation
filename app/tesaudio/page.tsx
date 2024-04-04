9d87c8aa5df8846f55b2f55bf63df94b0d5fb41c
Mochamad ilham alwi rifa <95983472+alwirifa@users.noreply.github.com>
2024-04-04 14:20:48 +0700

    tes

--- app/tesaudio/page.tsx
+++ app/tesaudio/page.tsx
@@ -35,7 +35,7 @@ const CustomYouTubePlayer = () => {
   const geofenceAreas: GeofenceArea[] = [
 
     // // gasmin
-    { latitude: -6.925584440785347, longitude: 107.6650065113677, radius: 12, videoId: "DOOrIxw5xOw" },
+    { latitude: -6.925589766062283, longitude: 107.66502931014318, radius: 20, videoId: "DOOrIxw5xOw" },
 
     { latitude: -6.9167608, longitude: 107.6616099, radius: 4, videoId: "XnUNOaxw6bs" },
 
