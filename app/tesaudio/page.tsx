c0927f04156b84012140cbecda8629db60d95f5e
Mochamad ilham alwi rifa <95983472+alwirifa@users.noreply.github.com>
2024-04-04 15:05:46 +0700

    tes

--- app/tesaudio/page.tsx
+++ app/tesaudio/page.tsx
@@ -3,8 +3,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import YouTube from 'react-youtube';
 import Navbar from '../components/Navbar';
-import { CircleSpinner } from 'react-spinner-overlay';
-
 interface UserLocation {
   latitude: number;
   longitude: number;
@@ -20,27 +18,26 @@ interface GeofenceArea {
 }
 
 const CustomYouTubePlayer = () => {
-  const [player, setPlayer] = useState<any>(null); 
+  const [player, setPlayer] = useState<any>(null);
   const [experienceStarted, setExperienceStarted] = useState(false);
   const [userLocation, setUserLocation] = useState<UserLocation | null>(null);
   const [watchId, setWatchId] = useState<number | null>(null);
   const [currentVideoId, setCurrentVideoId] = useState<string | null>(null);
   const [isPlaying, setIsPlaying] = useState(false);
   const [currentAreaIndex, setCurrentAreaIndex] = useState<number | null>(null);
-  const [isLoading, setIsLoading] = useState(true);
+  const [showPlayButton, setShowPlayButton] = useState(false);
   const playerRef = useRef<any>(null);
 
   const geofenceAreas: GeofenceArea[] = [
-    { latitude: -6.9166387, longitude: 107.6615271, radius: 8, videoId: "DOOrIxw5xOw" },
+    { latitude: -6.925572458911993, longitude: 107.66502260462097, radius: 15, videoId: "DOOrIxw5xOw" },
     { latitude: -6.9167608, longitude: 107.6616099, radius: 8, videoId: "XnUNOaxw6bs" },
     { latitude: -6.9167322, longitude: 107.6613635, radius: 8, videoId: "36YnV9STBqc" },
     { latitude: -6.9168766, longitude: 107.6614897, radius: 8, videoId: "bk8WKwHDUNk" },
     { latitude: -6.5168766, longitude: 107.7614897, radius: 8, videoId: 'yNKvkPJl-tg' }
-  ]
+  ];
 
   const watchUserLocation = () => {
     setExperienceStarted(true);
-    setIsLoading(true);
 
     if (navigator.geolocation) {
       const id = navigator.geolocation.watchPosition(
@@ -59,11 +56,15 @@ const CustomYouTubePlayer = () => {
             }
           });
 
-          setIsPlaying(true);
-          player.playVideo()
-
           setCurrentAreaIndex(isInsideAnyGeofence ? areaIndex : null);
+          setIsPlaying(isInsideAnyGeofence);
+          setShowPlayButton(!isInsideAnyGeofence);
 
+          if (player && isInsideAnyGeofence) {
+            player.playVideo();
+          } else if (player) {
+            player.pauseVideo();
+          }
         },
         (error) => {
           console.error('Error watching user location: ', error);
@@ -100,13 +101,11 @@ const CustomYouTubePlayer = () => {
   useEffect(() => {
     if (player && currentVideoId) {
       player.loadVideoById(currentVideoId);
-      player.playVideo();
       if (isPlaying) {
         player.playVideo();
       } else {
         player.pauseVideo();
       }
-      setIsLoading(false);
     }
   }, [player, currentVideoId, isPlaying]);
 
@@ -130,11 +129,11 @@ const CustomYouTubePlayer = () => {
     setPlayer(event.target);
   };
 
-  const playVideo = () => {
+  const GPS = () => {
     watchUserLocation();
   };
 
-  const startExperience = () => {
+  const playVideo = () => {
     setIsPlaying(true);
     player.playVideo();
   };
@@ -143,14 +142,13 @@ const CustomYouTubePlayer = () => {
     height: '100',
     width: '100',
     playerVars: {
-      autoplay: 1, 
+      autoplay: 0,
     },
   };
 
   return (
     <div className='h-[100svh] w-full relative '>
       <div className='bg-background h-[100svh] w-full bg-cover bg-center'>
-
         <div className='absolute top-0 flex justify-between w-full p-8'>
           <div>
             <img src="/images/ttd.png" alt="" className='w-auto h-8' />
@@ -171,78 +169,12 @@ const CustomYouTubePlayer = () => {
             />
           ))}
         </div>
-
-        {!experienceStarted ? (
-          <div className={`flex flex-col items-center justify-center w-full pt-[16svh]`}>
-            <div className='flex flex-col justify-center items-center gap-1'>
-              <p className='text-lg text-purple font-semibold'>HERE, NOWHERE HEAR</p>
-              <p className='text-xs'>Tomy Herseta, 2024.</p>
-            </div>
-            <div className='flex flex-col gap-4 p-8 mt-6 '>
-              <p className='text-justify font-medium'>das</p>
-              <p className='text-justify font-medium'>as</p>
-            </div>
-          </div>
-        ) : (
-          <div className="flex flex-col gap-4 items-center  pt-[16svh]">
-            <div className='flex flex-col justify-center items-center gap-1'>
-              <p className='text-lg text-purple font-semibold'>HERE, NOWHERE HEAR</p>
-              <p className='text-xs'>Tomy Herseta, 2024.</p>
-            </div>
-            <div className='relative'>
-              <div className='bg-purple  flex justify-end items-center px-3 py-1 absolute top-0 w-full'>
-                <img src="/images/close.png" className='h-[30px] w-[30px]' alt="" onClick={stopWatchUserLocation} />
-              </div>
-              <img src="/images/map.png" alt="" className='h-auto w-[300px]' />
-              <div className='bg-purple  flex justify-between items-center p-2 px-4 absolute bottom-0 w-full'>
-                {currentAreaIndex !== null ? (
-                  <p className="text-white font-semibold">AUDIO {currentAreaIndex}</p>
-                ) : (
-                  <p className="text-white font-semibold">OUT OF AREA</p>
-                )}
-                <div>
-                  <img src="/images/audio.png" alt="" className='h-6 w-6' />
-                </div>
-              </div>
-              {/* Your other elements... */}
-            </div>
-          </div>
+        <button onClick={GPS}>GPS</button>
+        {showPlayButton && (
+          <button className="bg-blue-500 text-white font-semibold py-2 px-4 rounded absolute bottom-4 left-4" onClick={playVideo}>Play Video</button>
         )}
-
-        {experienceStarted ? (
-          <div>
-            <div className='absolute w-full bottom-[14svh] flex flex-col gap-4 justify-center items-center'>
-              <img src='/images/headphones.png' alt='' className='h-24 w-24' />
-              <button
-                className='border border-purple text-purple font-semibold px-6 py-2 rounded-full max-w-max'
-                onClick={stopWatchUserLocation}
-              >
-                STOP LISTENING
-              </button>
-              {/* {isLoading && (
-                <div className='fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50'>
-                  <CircleSpinner color='#FFF' outerBorderOpacity={0.5} outerBorderWidth={3} innerBorderWidth={3} />
-                </div>
-              )} */}
-            </div>
-          </div>
-        ) : (
-          <div className='absolute w-full bottom-[14svh] flex flex-col gap-4 justify-center items-center'>
-            <img src='/images/headphones.png' alt='' className='h-24 w-24' />
-            <button
-              className='border border-purple text-purple font-semibold px-6 py-2 rounded-full max-w-max'
-              onClick={startExperience}
-            >
-              START EXPERIENCE
-            </button>
-            <button
-              className='border border-purple text-purple font-semibold px-6 py-2 rounded-full max-w-max'
-              onClick={playVideo}
-            >
-              WATCH USER LOCATION
-            </button>
-          </div>
-        )}
+        <p className="text-white font-semibold">AUDIO {currentAreaIndex}</p>
+        <img src="/images/close.png" className='h-[30px] w-[30px]' alt="" onClick={stopWatchUserLocation} />
 
         <div className='absolute bottom-0 w-full'>
           <Navbar />
