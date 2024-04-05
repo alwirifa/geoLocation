cabbcf516bfeebe5d27b1fd40c5d819c09845f4b
Mochamad ilham alwi rifa <95983472+alwirifa@users.noreply.github.com>
2024-04-05 15:13:43 +0700

    tes

--- app/tesaudio/page.tsx
+++ app/tesaudio/page.tsx
@@ -26,7 +26,7 @@ const CustomYouTubePlayer = () => {
   const [showPlayButton, setShowPlayButton] = useState(true);
   const [videoPlayed, setVideoPlayed] = useState(false);
   const [hasUserClicked, setHasUserClicked] = useState(false);
-
+  const [experienceStarted, setExperienceStarted] = useState(false);
 
 
 
@@ -38,6 +38,8 @@ const CustomYouTubePlayer = () => {
   ];
 
   const watchUserLocation = () => {
+
+    setExperienceStarted(true)
 
     if (navigator.geolocation) {
       const id = navigator.geolocation.watchPosition(
@@ -62,7 +64,7 @@ const CustomYouTubePlayer = () => {
 
             setIsPlaying(isInsideAnyGeofence);
           }
-         
+
         },
         (error) => {
           console.error('Error watching user location: ', error);
@@ -81,6 +83,7 @@ const CustomYouTubePlayer = () => {
 
 
   const stopWatchUserLocation = () => {
+    setExperienceStarted(false)
     if (watchId !== null) {
       navigator.geolocation.clearWatch(watchId);
       setWatchId(null);
@@ -153,6 +156,7 @@ const CustomYouTubePlayer = () => {
       : "HIRNdveLnJI";
 
     setCurrentVideoId(videoIdToPlay);
+    setShowPlayButton(false)
     setIsPlaying(true);
     setVideoPlayed(true);
     player.playVideo();
@@ -161,47 +165,128 @@ const CustomYouTubePlayer = () => {
 
   return (
     <div className='h-[100svh] w-full relative '>
+      <div className='h-[100svh] w-full relative '>
+        <div className='bg-background h-[100svh] w-full bg-cover bg-center'>
 
-      <div className='absolute top-0'>
-        {geofenceAreas.map((area, index) => (
-          <YouTube
-            key={index}
-            videoId={area.videoId}
-            onReady={onReady}
-            opts={opts}
-          />
-        ))}
-      </div>
-
-      <div className='absolute bottom-10 left-0 flex flex-col gap-4'>
-        {userLocation && (
-          <div>
-            <p>Latitude: {userLocation.latitude}</p>
-            <p>Longitude: {userLocation.longitude}</p>
-            <p>Accuracy: {userLocation.accuracy} meters</p>
+          <div className='absolute top-0 flex justify-between w-full p-8'>
+            <div>
+              <img src="/images/ttd.png" alt="" className='w-auto h-8' />
+            </div>
+            <div className='flex gap-4 items-center'>
+              <img src="/images/jakartaLogo.png" alt="" className='w-auto h-4' />
+              <img src="/images/forteLogo.png" alt="" className='w-auto h-4' />
+            </div>
           </div>
-        )}
-        {currentAreaIndex !== null ? (
-          <p className="font-semibold">AUDIO {currentAreaIndex + 1}</p>
-        ) : (
-          <p className="font-semibold">OUT OF AREA</p>
-        )}
-        {showPlayButton  ? (
-          <div>
-            <button className='p-4 border font-semibold' onClick={playVideo}>
-              Play Video 
+
+
+          <div className='absolute top-0'>
+            {geofenceAreas.map((area, index) => (
+              <YouTube
+                key={index}
+                videoId={area.videoId}
+                onReady={onReady}
+                opts={opts}
+              />
+            ))}
+          </div>
+
+          {showPlayButton ? (
+            <div className='w-full h-full flex justify-center items-center fixed top-0'>
+
+              <div className='border-2 p-4 rounded-md flex justify-center items-center flex-col gap-4'>
+                <p className='font-semibold'>Allow Your GPS location</p>
+                <button className='p-4 py-2 rounded-md border font-semibold shadow--md' onClick={playVideo}>
+                  ok
+                </button>
+              </div>
+            </div>
+          ) : (
+            <button className='hidden' onClick={playVideo}>
+              ieu eweuh
             </button>
-          </div>
-        ) : (
-          <button className='hidden' onClick={playVideo}>
-            ieu eweuh
-          </button>
-        )}
+          )}
+          
+          {experienceStarted ? (
+            <div className="flex flex-col gap-4 items-center  pt-[16svh]">
 
-        <div className='p-8' onClick={watchUserLocation}>
-          Watch User Location
+              <div className='flex flex-col justify-center items-center gap-1'>
+                <p className='text-lg text-purple font-semibold'>HERE, NOWHERE HEAR</p>
+                <p className='text-xs'>Tomy Herseta, 2024.</p>
+              </div>
+              <div className='relative'>
+                <div className='bg-purple  flex justify-end items-center px-3 py-1 absolute top-0 w-full'>
+                  <img src="/images/close.png" className='h-[30px] w-[30px]' alt="" onClick={stopWatchUserLocation} />
+                </div>
+                <img src="/images/map.png" alt="" className='h-auto w-[300px]' />
+                <div className='bg-purple  flex justify-between items-center p-2 px-4 absolute bottom-0 w-full'>
+                  {currentAreaIndex !== null ? (
+                    <p className="text-white font-semibold">AUDIO {currentAreaIndex + 1}</p>
+                  ) : (
+                    <p className="text-white font-semibold">OUT OF AREA</p>
+                  )}
+
+                  <div>
+                    <img src="/images/audio.png" alt="" className='h-6 w-6' />
+                  </div>
+                </div>
+
+                <div className='absolute bottom-[134px] right-16 p-4 border-2 border-green-500 flex justify-center items-center'>
+                  <div className={`bg-red-500 h-4 w-4 rounded-full animate-ping absolute ${currentAreaIndex === 0 ? 'visible' : 'hidden'}`} />
+                </div>
+                <div className='absolute top-36 right-24 p-4 border-2 border-green-500 flex justify-center items-center'>
+                  <div className={`bg-red-500 h-4 w-4 rounded-full animate-ping absolute ${currentAreaIndex === 1 ? 'visible' : 'hidden'}`} />
+                </div>
+                <div className='absolute top-20 left-32 p-4 border-2 border-green-500 flex justify-center items-center'>
+                  <div className={`bg-red-500 h-4 w-4 rounded-full animate-ping absolute ${currentAreaIndex === 2 ? 'visible' : 'hidden'}`} />
+                </div>
+                <div className='absolute top-32 left-16 p-4 border-2 border-green-500 flex justify-center items-center'>
+                  <div className={`bg-red-500 h-4 w-4 rounded-full animate-ping absolute ${currentAreaIndex === 3 ? 'visible' : 'hidden'}`} />
+                </div>
+
+                <div className='absolute bottom-40 left-4 p-4 border-2 border-green-500 flex justify-center items-center'>
+                  <div className={`bg-red-500 h-4 w-4 rounded-full animate-ping absolute ${currentAreaIndex === 4 ? 'visible' : 'hidden'}`} />
+                </div>
+
+                <div className='absolute top-16 right-8 p-4 border-2 border-green-500 flex justify-center items-center'>
+                  <div className={`bg-red-500 h-4 w-4 rounded-full animate-ping absolute ${currentAreaIndex === 5 ? 'visible' : 'hidden'}`} />
+                </div>
+              </div>
+            </div>
+
+          ) : (
+            <div>
+
+              <div className={`flex flex-col items-center justify-center w-full pt-[16svh]`}>
+
+                <div className='flex flex-col justify-center items-center gap-1'>
+                  <p className='text-lg text-purple font-semibold'>HERE, NOWHERE HEAR</p>
+                  <p className='text-xs'>Tomy Herseta, 2024.</p>
+                </div>
+
+                <div className='flex flex-col gap-4 p-8 mt-6 '>
+                  <p className='text-justify font-medium'>
+                    das
+                  </p>
+                  <p className='text-justify font-medium'>
+                    as
+                  </p>
+                </div>
+              </div>
+              <div className='absolute w-full bottom-[14svh] flex flex-col gap-4 justify-center items-center'>
+                <img src='/images/headphones.png' alt='' className='h-24 w-24' />
+                <button
+                  className='border border-purple text-purple font-semibold px-6 py-2 rounded-full max-w-max'
+                  onClick={watchUserLocation}
+                >
+                  START EXPERIENCE
+                </button>
+              </div>
+            </div>
+          )}
+
         </div>
       </div>
+
 
     </div>
   );
