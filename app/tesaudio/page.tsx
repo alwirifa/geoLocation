750714e3eef3e7b11fa5749c6f45d594db2a4c12
Mochamad ilham alwi rifa <95983472+alwirifa@users.noreply.github.com>
2024-04-05 14:06:51 +0700

    tes

--- app/tesaudio/page.tsx
+++ app/tesaudio/page.tsx
@@ -24,16 +24,16 @@ const CustomYouTubePlayer = () => {
   const [isPlaying, setIsPlaying] = useState(false);
   const [currentAreaIndex, setCurrentAreaIndex] = useState<number | null>(null);
   const [showPlayButton, setShowPlayButton] = useState(false);
-  const [videoPlayed, setVideoPlayed] = useState(false); // Track whether video has been played or not
-    const [hasUserClicked, setHasUserClicked] = useState(false); // Track whether user has clicked on video
+  const [videoPlayed, setVideoPlayed] = useState(false);
+  const [hasUserClicked, setHasUserClicked] = useState(false);
 
 
 
 
   const geofenceAreas: GeofenceArea[] = [
-    { latitude:  -6.925391401199705,  longitude: 107.66489758575915, radius: 10, videoId: "XnUNOaxw6bs" },
+    { latitude: -6.925391401199705, longitude: 107.66489758575915, radius: 10, videoId: "XnUNOaxw6bs" },
     { latitude: -6.925487185695368, longitude: 107.66453954172253, radius: 10, videoId: "36YnV9STBqc" },
-    { latitude: -6.925579012135751,  longitude: 107.66500683304874, radius: 8, videoId: "bk8WKwHDUNk" },
+    { latitude: -6.925579012135751, longitude: 107.66500683304874, radius: 8, videoId: "bk8WKwHDUNk" },
     { latitude: -6.5168766, longitude: 107.7614897, radius: 8, videoId: 'yNKvkPJl-tg' }
   ];
 
@@ -62,8 +62,8 @@ const CustomYouTubePlayer = () => {
 
             setIsPlaying(isInsideAnyGeofence);
           }
-          setShowPlayButton(isInsideAnyGeofence)
 
+          setShowPlayButton(isInsideAnyGeofence)
         },
         (error) => {
           console.error('Error watching user location: ', error);
@@ -141,24 +141,25 @@ const CustomYouTubePlayer = () => {
       const { videoId } = geofenceAreas[currentAreaIndex];
       setCurrentVideoId(videoId);
     } else {
-      setCurrentVideoId("6YaaRmXtJu0"); // Set videoId to "6YaaRmXtJu0" when out of area
+      setCurrentVideoId("VXcXdMgU0");
     }
   }, [currentAreaIndex]);
-  
+
   const playVideo = () => {
     setHasUserClicked(true)
-   
+
+
     const videoIdToPlay = currentAreaIndex !== null && geofenceAreas[currentAreaIndex]
       ? geofenceAreas[currentAreaIndex].videoId
-      : "6YaaRmXtJu0"; // Set videoId to "6YaaRmXtJu0" when out of area
-  
+      : "VXcXdMgU0";
+
     setCurrentVideoId(videoIdToPlay);
     setIsPlaying(true);
     setVideoPlayed(true); // Set videoPlayed to true when video starts playing
     setShowPlayButton(false); // Set showPlayButton to false when video starts playing
     player.playVideo();
   };
-  
+
 
   return (
     <div className='h-[100svh] w-full relative '>
