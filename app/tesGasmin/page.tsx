bd0e775e768ef40d6a08d373c5f751d53e7f93a0
Mochamad ilham alwi rifa <95983472+alwirifa@users.noreply.github.com>
2024-04-18 11:39:13 +0700

    tes

--- app/tesGasmin/page.tsx
+++ app/tesGasmin/page.tsx
@@ -1,63 +1,60 @@
-"use client";
+"use client"
 
 import React, { useState, useEffect } from 'react';
 import Pizzicato from 'pizzicato';
 
-const Page = () => {
-  const [isPlaying, setIsPlaying] = useState(false);
+const MusicPlayer: React.FC = () => {
+  const [gain, setGain] = useState(0.4);
   const [sound, setSound] = useState<Pizzicato.Sound | null>(null);
+  const [playButton, setPlayButton] = useState(false)
 
   useEffect(() => {
-    const initializeSound = () => {
-      const newSound = new Pizzicato.Sound('/music/music.mp3', () => {
-      
-        // Create a reverb effect
-        const reverb = new Pizzicato.Effects.Reverb({
-          time: 3,
-          decay: 2,
-          reverse: false,
-          mix: 0.7
-        });
-
-        // Create a distortion effect
-        const distortion = new Pizzicato.Effects.Distortion({
-          gain: 0.4
-        });
-
-        // Add effects to the sound
-        newSound.addEffect(reverb);
-        newSound.addEffect(distortion);
-
-        // Set the sound
-        setSound(newSound);
+    const sound = new Pizzicato.Sound('/music/music.mp3', () => {
+      const distortion = new Pizzicato.Effects.Distortion({
+        gain: gain,
       });
-    };
-
-    initializeSound();
+      sound.addEffect(distortion);
+      setSound(sound);
+    });
 
     return () => {
-      if (sound) {
-        sound.stop();
-      }
+      sound && sound.stop();
     };
-  }, []); 
+  }, []);
 
-  const togglePlay = () => {
-    if (sound) {
-      if (isPlaying) {
-        sound.pause();
-      } else {
-        sound.play();
-      }
-      setIsPlaying(!isPlaying);
+  const handlePlay = () => {
+    setPlayButton(!playButton)
+
+    if (playButton) {
+      sound && sound.play();
+    } else {
+      sound && sound.pause();
+    }
+    console.log(playButton)
+  };
+
+  const handleSliderChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newGain = parseFloat(event.target.value);
+    setGain(newGain);
+    if (sound && sound.effects[0]) {
+      (sound.effects[0] as Pizzicato.Effects.Distortion).gain = newGain;
     }
   };
+  
 
   return (
     <div>
-      <button onClick={togglePlay}>{isPlaying ? 'Pause Music' : 'Play Music'}</button>
+      <button onClick={handlePlay}>Play</button>
+      <input
+        type="range"
+        min="0"
+        max="1"
+        step="0.1"
+        value={gain}
+        onChange={handleSliderChange}
+      />
     </div>
   );
 };
 
-export default Page;
+export default MusicPlayer;
